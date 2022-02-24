ba36333deb32d9f8b871b03112966b7fa100ae34
PratikChitruk <pratik.chitruk@gmail.com>
2022-02-24 12:30:09 +0530

    RollADice push for S&l

---
+++ RollADice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SankeLader
+{
+    public class RollADice
+    {
+        public static void RollingTheDice()
+        {
+
+
+            Random rand = new Random();
+            int dice = rand.Next(1, 6);
+
+            Console.WriteLine("no. is :" + dice);
+
+
+
+
+
+        }
+    }
+}
