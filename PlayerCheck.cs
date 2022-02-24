66ea28ead0f529e2bbe342aaeed166de5f3fcd69
PratikChitruk <pratik.chitruk@gmail.com>
2022-02-24 12:34:01 +0530

    playerChecks push for S&l

---
+++ PlayerCheck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnkeAndLadder
+{
+    public class PlayerCheck
+    {
+        public static string  ThePlayerIsChecking()
+        {   
+
+            Random rand = new Random();
+            int dice = rand.Next(7);
+
+            Console.WriteLine("no. is :" + dice);
+
+            string[] square = { " NO MOVE", "GO AHEAD", "CLIMB LADDER", "GO AHEAD", "GO AHEAD", "DOWN BY SNAKE", "GO AHEAD", "GO HEAD", };
+
+            return square[dice];
+
+            Console.WriteLine(" the sqare id display");
+        }
+       
+    }
+}
+
