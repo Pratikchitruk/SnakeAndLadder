23a52655f103513430c806eefd75b9812e38f5ee
PratikChitruk <pratik.chitruk@gmail.com>
2022-02-24 12:23:11 +0530

    program push for S&l

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SankeLader
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to snake and ladder game");
+            RollADice.RollingTheDice();
+
+             string square = SnkeAndLadder.PlayerCheck.ThePlayerIsChecking();
+            Console.WriteLine("The sqare is" + square );
+            WiningPlayer.ThePlayerIsWining();
+           
+
+
+
+        }
+    }
+}
